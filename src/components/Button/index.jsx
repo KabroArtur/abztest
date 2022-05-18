5e01d45bbc8f8ed9a6493f78dd3fb940729e3279
KabroArtur <78974590+KabroArtur@users.noreply.github.com>
2022-05-19 02:25:14 +0300

    Latest

--- src/components/Button/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './button.scss';
-
-export function Button(props){
-    return(
-        <button className={props.className} onClick={props.onClick} id={props.id} disabled={props.disabled}>{props.text}</button>
-    );
-}
